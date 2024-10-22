3d3e8a7f97def53fdd5b72905957dc31256a3a41
Rachel Crafford <112960619+rachelcrafford@users.noreply.github.com>
2024-10-22 14:44:31 -0600

    Add files via upload

---
+++ code_for_extracting_veg.R
@@ -0,0 +1,144 @@
+
+
+## This script outlines the workflow for deriving vegetation structure metrics from LiDAR datasets. 
+
+# Install and load the required packages
+install.packages("lidR")
+library(lidR)
+install.packages("terra")
+library(terra)
+install.packages("raster")
+library(raster)
+install.packages("sf")
+library(sf)
+install.packages("dplyr")
+library(dplyr)
+install.packages("future")
+library(future)
+
+## Set working directory
+setwd("G:/My Drive/GMUG_LiDAR/data")
+
+#############################################
+
+## Loading and preparing data
+
+# Load in GMUG shapefile 
+gmug <- st_read("G:/.shortcut-targets-by-id/18KzRJgC9b5miNvjWohrVBh1U8Mq9tdVP/GMUG_LiDAR/data/masks/gmug3.shp")
+
+# Choose a specific variable to keep
+Shape_Area <- "Shape_Area"
+gmug <- gmug[, "Shape_Area", drop = FALSE]
+
+# Load in Raster Template
+
+raster_template_3m <- rast("G:/.shortcut-targets-by-id/18KzRJgC9b5miNvjWohrVBh1U8Mq9tdVP/GMUG_LiDAR/data/masks/GB_mask_3m_v1.tif")
+crs(raster_template_3m)
+
+# Load in LiDAR point cloud data 
+
+mesa <- readLAScatalog("G:/.shortcut-targets-by-id/18KzRJgC9b5miNvjWohrVBh1U8Mq9tdVP/GMUG_LiDAR/data/mesa/")
+
+# Check and display the CRS of the LiDar catalog and the polygon
+st_crs(mesa)
+st_crs(gmug)
+
+# If the CRS doesn't match, re-project one of them to the other's CRS
+gmug <- st_transform(gmug, st_crs(mesa))
+
+# Clip LAS files to GMUG extent 
+mesa <- catalog_intersect(mesa, gmug) 
+
+# Check out the clip to ensure that the LAS files overlap our area of interest
+plot(mesa)
+plot(gmug, add=TRUE, col=rgb(0.7,0.5,0.5,0.5))
+
+
+## Preprocessing our data
+
+# Catalog engine processes by "chunks", which might not be the same as the underlying files.
+opt_chunk_size(mesa) <- 1000
+opt_chunk_buffer(mesa) <- 30 # space around tile edges
+opt_stop_early(mesa) <- FALSE
+opt_output_files(mesa) <- ("G:/.shortcut-targets-by-id/18KzRJgC9b5miNvjWohrVBh1U8Mq9tdVP/GMUG_LiDAR/scratch/chunks/chunk_{ID}")
+
+
+# Sets catalog to overwrite outputs by default.
+mesa@output_options$drivers$Raster$param$overwrite <- TRUE
+
+## Defining custom metrics and processing functions
+
+# Custom metrics function
+process_las_catalog <- function(z, i, r) { # user-defined function. can add metrics based on interest
+  list(
+    z80 = quantile(z, probs=0.80), # heights above 80th percentile
+    z_mean = mean(z), # mean height
+    coef_var = sd(z) / mean(z) * 100, # coefficient of variation
+    canopy_cover = length(z[z > 2.0]) / length(z),
+    understory_cover = length(z[z > 1.0 & z <= 3.0]) / length(z[z <= 3.0]) # adjust understory based on vegetation in area of interest
+  )  
+}
+
+# Custom catalog processing function.
+myfun <- function(chunk,template_path="G:/.shortcut-targets-by-id/18KzRJgC9b5miNvjWohrVBh1U8Mq9tdVP/GMUG_LiDAR/data/masks/GB_mask_3m_v1.tif") {
+  set_lidr_threads(1L)
+  # Load the chunk + buffer
+  las <- readLAS(chunk)
+  if (is.empty(las)) return(NULL)
+  
+  # New function to re-project LiDAR point cloud
+  raster_template_3m <- rast(template_path)
+  las_reproj <- sf::st_transform(las, st_crs(raster_template_3m))
+  
+  # Normalize and filter point cloud
+  norm <- normalize_height(las_reproj,algorithm=tin())
+  filt <- filter_poi(norm, Z < 55 & Z > 0)
+  
+  # Compute custom pixel metrics
+  custommetrics <- pixel_metrics(filt, 
+                                      ~process_las_catalog(z = Z, 
+                                                           i = Intensity, 
+                                                           r = ReturnNumber),
+                                      res=10)
+  return(custommetrics)
+}
+
+# Runs function on catalog
+plan(multisession, workers = 8L)
+mesa_veg <- catalog_apply(mesa, myfun)
+
+## Exporting results
+
+# Convert list elements to SpatRaster
+rast_list <- list()
+for (i in 1:length(mesa_veg)) {
+  rast_list[[i]] <- rast(mesa_veg[[i]])
+}
+
+# Create spat raster collection 
+mesa_sprc <- sprc(rast_list)
+
+# Merge spat rasters
+mesa_mos <- merge(mesa_sprc)
+plot(mesa_mos)
+
+
+# Export individual plots 
+z80 <- (mesa_mos[[1]])
+zmean <- (mesa_mos[[2]])
+coefvar <- (mesa_mos[[3]])
+canopycov <- (mesa_mos[[4]])
+understorycov <- (mesa_mos[[5]])
+
+# Save to disk
+writeRaster(understorycov, filename = 'G:/.shortcut-targets-by-id/18KzRJgC9b5miNvjWohrVBh1U8Mq9tdVP/GMUG_LiDAR/deliverables/Block 5_2//understorycov.tif',
+            overwrite=TRUE)
+writeRaster(canopycov, filename = 'G:/.shortcut-targets-by-id/18KzRJgC9b5miNvjWohrVBh1U8Mq9tdVP/GMUG_LiDAR/deliverables/Block 5_2/canopycov.tif',
+            overwrite=TRUE)
+writeRaster(coefvar, filename = 'G:/.shortcut-targets-by-id/18KzRJgC9b5miNvjWohrVBh1U8Mq9tdVP/GMUG_LiDAR/deliverables/Block 5_2/coefvar.tif',
+            overwrite=TRUE)
+writeRaster(zmean, filename = 'G:/.shortcut-targets-by-id/18KzRJgC9b5miNvjWohrVBh1U8Mq9tdVP/GMUG_LiDAR/deliverables/Block 5_2/zmean.tif',
+            overwrite=TRUE)
+writeRaster(z80, filename = 'G:/.shortcut-targets-by-id/18KzRJgC9b5miNvjWohrVBh1U8Mq9tdVP/GMUG_LiDAR/deliverables/Block 5_2/z80.tif',
+            overwrite=TRUE)
+
